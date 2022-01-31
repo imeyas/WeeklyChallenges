1f200357ed37ca48d14e761921c81a3b6bca4844
Ebo Onwunali <ebo@njgeneral.com>
2022-01-30 19:41:09 -0500

    Finished methods for Set2 and some Set3

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,8 +8,8 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            var isTrue = 'C';
-            return true;
+            return char.IsLetter(c);
+           
 
             //throw new NotImplementedException();
         }
@@ -27,7 +28,7 @@ namespace ChallengesWithTestsMark8
             }
 
 
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public bool IsNumberEven(int number)
@@ -61,32 +62,128 @@ namespace ChallengesWithTestsMark8
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            double max = 0;
+            double min = 10000;
+
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+
+            foreach ( double number in numbers)
+            {
+                if(number > max)
+                {
+                    max = number;
+                }
+            }
+
+            foreach (double number in numbers)
+            {
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+
+                return min + max;
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            if(str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+            else
+            {
+                return str2.Length;
+            }
+            
+
+
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+                //throw new NotImplementedException();
+
+                var sum = 0;
+
+            if(numbers == null)
+            {
+                return 0;
+            }
+
+            foreach (int number in numbers)
+                if (number >= 0)
+                {
+                    sum += number;
+                }
+               
+            foreach (int number in numbers)
+                if (number < 0)
+                {
+                    sum += number;
+                }
+
+            return sum;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            var evensSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evensSum += numbers[i];
+                }
+            }
+
+            return evensSum;
+
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            if (numbers == null)
+            {
+                return false;
+            }
+
+            return numbers.Sum() % 2 != 0;
+
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            if (number <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return number / 2;
+            }
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -7,11 +7,61 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
+            for (int i = 0; i < vals.Length; i++)
+            {
+                if (vals[i] == false)
+                {
+                    return true;
+                }
+                else if (vals[i] == true)
+                {
+                    return false;
+                }
+                
+            }
+
+            foreach (bool val in vals)
+            {
+                if (val == false)
+                { return true; }
+                else if (val == true)
+                { return false;}
+            }
+
+            return true;
+
+
+            //for (int i = 0; i < 0; i++)
+            //{
+            //    if (vals[i] == true || vals[i] == false)
+            //    {
+            //        return vals[i];
+            //    }
+            //}
+            //return true;
+            //return false;
+
             throw new NotImplementedException();
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
+
+            foreach (int i in numbers)
+            {
+                int sum = 0;
+                for (int j = 0; j < sum; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        return true;
+                    }
+                    //else
+                    //{ return true; }
+                }
+            }
+
+            return false;
             throw new NotImplementedException();
         }
 
@@ -22,17 +72,43 @@ namespace ChallengesWithTestsMark8
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            char firstLetter = val[0];
+            return firstLetter;
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            char lastLetter = val[val.Length - 1];
+
+            return lastLetter;
+
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return 0;
+            }
+
+            decimal quotient = dividend / divisor;
+            return quotient;
+
+
+
+            //try
+            //{
+            //    quotient = dividend / divisor;
+            //    return quotient;
+            //}
+            //catch
+            //{
+            //    DivideByZeroException exception;
+            //}
+            //throw new NotImplementedException();
         }
 
         public int LastMinusFirst(int[] nums)
@@ -42,7 +118,19 @@ namespace ChallengesWithTestsMark8
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            int[] odds = new int[50];
+            int counter = 0;
+            for (int i = 1; i < 100; i += 2)
+            {
+                odds[counter] = i;
+                counter++;
+            }
+
+            return odds;
+
+
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
